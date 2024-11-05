9fd08c203b9f402d8daed2c99830146c45de191b
georgia <geaoneill@googlemail.com>
2024-11-05 09:19:13 +0000

    allow users to view single article on separate page

--- api.js
+++ api.js
@@ -7,3 +7,12 @@ export function getArticles() {
       return results.data.articles;
     });
 }
+
+export function getArticleById(article_id) {
+  return axios
+    .get(`https://be-nc-news-osc4.onrender.com/api/articles/${article_id}`)
+    .then((results) => {
+      console.log(results.data);
+      return results.data.article;
+    });
+}
--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { getArticles } from "../../api";
 
 export default function ArticlesList() {
-  const [articles, setArticles] = useState([]);
+  const [articlesList, setArticlesList] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     getArticles().then((results) => {
       setLoaded(true);
-      setArticles(results);
+      setArticlesList(results);
     });
   }, []);
 
@@ -16,16 +17,18 @@ export default function ArticlesList() {
     {
       return (
         <section className="articles">
-          {articles.map((article) => {
+          {articlesList.map((article) => {
             return (
               <section key={article.article_id} className="article">
-                <p>{article.title}</p>
+                <Link to={`/articles/${article.article_id}`}>
+                  {article.title}
+                </Link>
                 <img src={article.article_img_url} style={{ width: "70%" }} />
-                <p>{article.author}</p>
-                <p>{article.topic}</p>
-                <p>{article.votes}</p>
-                <p>{article.comment_count}</p>
-                <p>{article.created_at}</p>
+                <p>Written by: {article.author}</p>
+                <p>Topic: {article.topic}</p>
+                <p>Votes: {article.votes}</p>
+                <p>Comments: {article.comment_count}</p>
+                <p>Created: {article.created_at}</p>
               </section>
             );
           })}
